d1b4295e515981e28fc8e1a8e5689aefb9f9b9d4
Sara Kim <sarakim0903@gmail.com>
2018-07-12 15:45:03 +0000

    Done.

---
+++ pract.rb
@@ -0,0 +1,7 @@
+sandwiches = [["ham", "swiss"], ["turkey", "cheddar"], ["roast beef", "gruyere"]]
+
+sandwiches.each do |subarray|
+  subarray.each do |element|
+	puts "#{element}"
+end
+end
--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,11 +1,13 @@
 
 
 def create_olympics_hash
-  {Sydney: "2000",
-  Athens: "2004",
-  Beijing: "2008",
-  London: "2012"}
+  {:Sydney => "2000",
+  :Athens => "2004",
+  :Beijing => "2008",
+  :London => "2012"}
 end
+
+
 
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
@@ -13,17 +15,27 @@ def add_a_key_value_pair
   new_hash[:Atlanta] = "1996"
   new_hash
 end
+  
+  
+
+
 
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
   
-  add_a_key_value_pair.each {|key,value|
+  add_a_key_value_pair.each do |key,value|
     puts "The #{key} summer olympics took place in #{value}."
-  }
+  
 end
+  
+end
+iterate_through_hash
+
+
 
 def iterate_through_keys
   upcased_cities = add_a_key_value_pair.map {|key, value| key.upcase  }
   upcased_cities.each {|city| puts city}
 end
+
